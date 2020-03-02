5a98c8f8b6403b5e19fe4e91f1a8814138bfe848
Gary Taylor <gary@Garys-Mac-mini.local>
2020-03-02 12:14:30 -0800

    better split up segments, freeze+playback

--- scripts/split_video_with_timestamp.sh
+++ scripts/split_video_with_timestamp.sh
@@ -16,9 +16,19 @@ wt=$4
 leadtime=$5
 bitrate=$6
 
+posttime=5
 totaltime=$(($wt + $leadtime))
+videotime=$(($totaltime + $posttime))
 ss=$(echo - | awk -v sowt="$sowt" -v leadtime="$leadtime" '{print sowt - leadtime}')
 
 echo 'Getting Duration'
 duration=$(ffprobe -loglevel error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$input")
-ffmpeg -f mpeg -ss $ss -i "$input" -an -t $totaltime $bitrate -c:v h264_omx -vf "drawtext=fontfile=OpenSans-Regular.ttf:text='%{eif\\:if(between(t\, 0\, $leadtime)\, $wt\, (($duration + $totaltime + .9) - $duration)-t)\\:d\\:2}':fontcolor=white:fontsize=24:x=w-tw-20:y=th:box=1:boxcolor=black@0.5:boxborderw=10" "$output.mp4"
+ffmpeg -f mpeg -ss $ss -i "$input" -an -t $videotime $bitrate -c:v h264_omx -vf " \
+drawtext=fontfile=DejaVuSans.ttf:fontcolor=black:fontsize=96:x=20:y=(h-text_h) - 20 \
+:enable='lt(t\, $leadtime)':text='%{eif\\:$wt\\:d\\:2}' \
+,drawtext=fontfile=DejaVuSans.ttf:fontcolor=black:fontsize=96:x=20:y=(h-text_h) - 20 \
+:enable='gte(t\, $leadtime)':text='%{eif\\:if(between(t\, $leadtime, $totaltime)\, (($duration + $totaltime + .9) - $duration)-t\, 0)\\:d\\:2}' \
+,drawtext=fontfile=DejaVuSans.ttf:fontcolor=black:fontsize=96:x=(w-text_w)-20:y=(h-text_h) - 20 \
+:enable='between(t\, $totaltime, $duration)':text='15/16' \
+,setpts='PTS-STARTPTS + gte(T,$totaltime)*(3/TB)' \
+" "$output.mp4"
