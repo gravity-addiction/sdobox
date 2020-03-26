ef4d85fcef9aff379ec57705c84ab5246220ca3d
Gary Taylor <gary@Garys-Mac-mini.local>
2020-03-26 10:48:01 -0700

    debug install-init

--- scripts/install-init.sh
+++ scripts/install-init.sh
@@ -9,6 +9,6 @@ gpio=4=op,dh
 gpio=12=op,dh' | sudo tee -a /boot/config.txt
 
 echo "dwc_otg.lpm_enable=0 $(cat /boot/cmdline.txt) fbcon=map:10 fbcon=font:VGA8x8 logo.nologo loglevel=3 vt.global_cursor_default=0" | sudo tee /boot/cmdline.txt
-sudo wget https://raw.githubusercontent.com/gravity-addiction/sdobox/tree/master/scripts/overlays/waveshare35c-overlay.dtb /boot/overlays/waveshare35c.dtbo
+sudo wget -O /boot/overlays/waveshare35c.dtbo https://raw.githubusercontent.com/gravity-addiction/sdobox/master/scripts/overlays/waveshare35c-overlay.dtb 
 
 
