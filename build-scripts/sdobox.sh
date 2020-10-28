b5c6d9816d4e235e2422df1948c52e577d56c6c8
Gary Taylor <gravity.addiction@gmail.com>
2020-10-27 21:17:49 -0700

    break out apt installer

--- build-scripts/sdobox.sh
+++ build-scripts/sdobox.sh
@@ -20,8 +20,9 @@ if [ ! -z $init ]; then
   git submodule init
   git submodule update
 
+  ./api-install.sh
   # Install Extra SDOBOX Libraries
-  sudo apt install -y autoconf libtool libtool-bin libsdl-ttf2.0-0 libts0 libconfig9 fonts-noto-mono git libulfius2.5 libulfius-dev libxdo-dev libconfig-dev libsdl-ttf2.0-dev libsqlite3-dev libiw-dev libmpv-dev wmctrl uwsc libulfius-dev libavahi-client-dev libavahi-common-dev libavahi-common-data libavahi-glib1 basez socat libxdo-dev libconfig-dev
+  # sudo apt install -y autoconf libtool libtool-bin libsdl-ttf2.0-0 libts0 libconfig9 fonts-noto-mono git libulfius2.5 libulfius-dev libxdo-dev libconfig-dev libsdl-ttf2.0-dev libsqlite3-dev libiw-dev libmpv-dev wmctrl uwsc libulfius-dev libavahi-client-dev libavahi-common-dev libavahi-common-data libavahi-glib1 basez socat libxdo-dev libconfig-dev
 
 fi
 
