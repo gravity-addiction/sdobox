341b86b8ebd7693bc2fb6a081286c2edcf6872df
Gary Taylor <gravity.addiction@gmail.com>
2020-11-03 14:53:23 -0800

    move automount exe in sdob scripts

--- scripts/automount-init.sh
+++ scripts/automount-init.sh
@@ -4,21 +4,6 @@
 sudo apt install -y exfat-fuse
 sudo apt install -y exfat-utils pmount
 
-echo '#!/usr/bin/env bash
-
-PART=$1
-FS_TYPE=`lsblk -o name,fstype | grep ${PART} | awk '{print $2}'`
-
-if [ ${FS_TYPE} = "exfat" ]
-then
-  sudo /bin/mkdir -p /media/pi/${PART}
-  sudo /bin/mount -t exfat /dev/${PART} /media/pi/${PART}
-else
-  /usr/bin/pmount --umask 000 --noatime -w --sync /dev/${PART} /media/${PART}
-fi' | sudo tee /usr/local/bin/automount
-sudo chmod 755 /usr/local/bin/automount
-
-
 echo "ACTION==\"add\", KERNEL==\"sd[a-z][0-9]\", TAG+=\"systemd\", ENV{SYSTEMD_WANTS}=\"usbstick-handler@%k\"'" | sudo tee /etc/udev/rules.d/usbstick.rules
 sudo chmod 644 /etc/udev/rules.d/usbstick.rules
 
