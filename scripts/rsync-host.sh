192a141471b7c7d33869e84cf9d70841e7b52969
Gary Taylor <gravity.addiction@gmail.com>
2020-11-04 07:53:45 -0800

    update host rsync

---
+++ scripts/rsync-host.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/usr/bin/rsync -avz --progress ~/Videos3 pi@sandyclaws.local:~/Videos/2020-GHOST-CF
--- scripts/rsync-usb.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-USBDRIVE=$1
-/usr/bin/rsync -avz --progress "~/Videos3" "pi@sandyclaws.local:/media/pi/${USBDRIVE}/Videos"
